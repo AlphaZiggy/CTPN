7e014ecec0b7e744d4c5821afc9e3d27a45be58a
AlphaZiggy <ziggy.alpha@gmail.com>
2017-05-18 06:33:10 +0000

    fix bug

--- tools/demo.py
+++ tools/demo.py
@@ -60,10 +60,10 @@ for im_name in demo_imnames:
     for box in text_lines:
         print box
     for box in text_lines:
-        margin_updown = (box[3] - box[0])*0.25
+        margin_updown = (box[3] - box[1])*0.25
         margin_leftright = margin_updown*2
-        box[0] = box[0] - margin_updown
-        box[2] = box[2] + margin_updown
+        box[0] = box[0] - margin_leftright
+        box[2] = box[2] + margin_leftright
         box[1] = box[1] - margin_updown
         box[3] = box[3] + margin_updown
     for box in text_lines:
